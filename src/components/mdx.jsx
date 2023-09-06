ddca67a8e1b5c79980b64af1eefad5025e8b8262
Miloš Petrović <milosptr@hotmail.com>
2023-09-06 15:05:25 +0000

    Update mdx.jsx

--- src/components/mdx.jsx
+++ src/components/mdx.jsx
@@ -44,7 +44,7 @@ export const img = function Img(props) {
 
 function ContentWrapper({ className, children }) {
   return (
-    <div className='mx-auto max-8xl px-6 lg:flex lg:px-12'>
+    <div className='mx-auto max-w-8xl px-6 lg:flex lg:px-12'>
       <div className='lg:ml-96 lg:flex lg:w-full lg:justify-end lg:pl-32'>
         <div className={twMerge('mx-auto max-w-lg lg:mx-0 lg:w-0 lg:max-w-4xl lg:flex-auto', className)}>
           {children}
