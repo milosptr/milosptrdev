d5c5e3487a563d7ed7f1720a91f9c27827521e17
Miloš Petrović <milosptr@hotmail.com>
2025-01-15 22:44:42 +0000

    Update WorkBox.jsx

--- src/components/boxes/WorkBox.jsx
+++ src/components/boxes/WorkBox.jsx
@@ -10,7 +10,7 @@ export const WorkBox = () => {
       <div className='flex items-center gap-3 font-semibold text-zinc-900 dark:text-zinc-100'>
         <WorkIcon className='h-6 w-6' />
         <div className='mt-0.5 leading-none'>{t('titles.work')}</div>
-        <div className='ml-auto text-sm font-medium text-zinc-900 dark:text-zinc-100'>6+ {t('general.years')}</div>
+        <div className='ml-auto text-sm font-medium text-zinc-900 dark:text-zinc-100'>8+ {t('general.years')}</div>
       </div>
       <ol className='mt-6 space-y-4 pl-[0!important]'>
         <WorkListItem
@@ -52,7 +52,7 @@ export const WorkBox = () => {
       </ol>
       <a
         className='group mt-6 inline-flex w-full items-center justify-center gap-2 rounded-md bg-zinc-50 px-3 py-2 text-sm font-medium text-zinc-900 outline-offset-2 transition hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 active:transition-none dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70'
-        href='/cv/MilosPetrovicCV-05-2023.pdf'
+        href='/cv/MilosPetrovicCV2025.pdf'
         download>
         {t('general.download_cv')}
         <DownloadIcon className='h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50' />
